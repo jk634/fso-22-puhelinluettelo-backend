cb4d6e4995c9370103cda5a055d0b6319a8f26fb
Jussi <jussi.kanto@tuni.fi>
2022-09-01 14:00:28 +0300

    Add 3.7 - puhelinluettelon backend, step7

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const { response } = require('express');
 const express = require('express');
+var morgan = require('morgan');
+
 const app = express();
 
 app.use(express.json());
+app.use(morgan('tiny'));
 
 let persons = [
   {
