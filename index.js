b1740e3d3993d212a526ebb4a9deda50a369a856
Jussi <jussi.kanto@tuni.fi>
2022-09-02 10:27:05 +0300

    Add 3.9 - puhelinluettelon backend, step9

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const { response } = require('express');
 const express = require('express');
 var morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 
