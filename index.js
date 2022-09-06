d391b3c49a902e358d9803b502190f69d14f547d
Jussi <jussi.kanto@tuni.fi>
2022-09-06 11:20:35 +0300

    Add 3.22 - lint-konfiguraatio

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.static('build'));
 app.use(cors());
 app.use(express.json());
 
-morgan.token('body', (req, res) => JSON.stringify(req.body));
+morgan.token('body', (req) => JSON.stringify(req.body));
 
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
@@ -43,7 +43,7 @@ app.get('/info', (req, res) => {
 
 app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
-    .then((result) => {
+    .then(() => {
       response.status(204).end();
     })
     .catch((error) => next(error));
